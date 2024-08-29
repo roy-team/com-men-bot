669e6c66eda47a48079ad2ecae06dc27790d3be2
back-royteam <122016338+back-royteam@users.noreply.github.com>
2024-08-29 12:39:31 +0300

    Fix output error

--- src/plugins/openai.ts
+++ src/plugins/openai.ts
@@ -6,19 +6,32 @@
  */
 
 import OpenAI from 'openai'
+import type { APIError } from 'openai/error'
 
 export default async function (content: string): Promise<string | null> {
   const client = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   })
 
-  const chatCompletion = await client.chat.completions.create({
-    messages: [{
-      role: 'user',
-      content
-    }],
-    model: process.env.OPENAI_MODEL ?? 'gpt-3.5-turbo',
-  })
+  try {
+    const chatCompletion = await client.chat.completions.create({
+      messages: [{
+        role: 'user',
+        content
+      }],
+      model: process.env.OPENAI_MODEL ?? 'gpt-3.5-turbo',
+    })
 
-  return chatCompletion.choices[0].message.content ?? null
+    return chatCompletion.choices[0].message.content ?? null
+  } catch (error: unknown) {
+    if (isAPIError(error)) {
+      return `OpenAI Error: ${error.message}`
+    }
+
+    return 'OpenAI Unknown Error'
+  }
+}
+
+function isAPIError(candidate: unknown): candidate is APIError {
+  return !!(candidate && typeof candidate === 'object' && 'request_id' in candidate)
 }
